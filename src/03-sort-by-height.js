c26c2b00983660fab749ae29aa2c2572fd7dba42
Nikolai <vova1988open@gmail.com>
2021-04-13 01:24:39 +0300

    feat: implement task 6'

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,9 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const result = arr.sort((a, b) => a - b);
+  return result;
 }
 
 module.exports = sortByHeight;
